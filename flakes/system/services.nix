c553a123fc89790dd69e6a3ca08d207f0fef7c7c
David Lee <dav@davlee.com>
2025-02-26 16:03:33 +1100

    tidies --gnome triple buffering patch no worky

--- flakes/system/services.nix
+++ flakes/system/services.nix
@@ -6,21 +6,6 @@
   tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
 in {
 
- # nixpkgs.overlays = [
- #    # GNOME 46: triple-buffering-v4-46
- #    (final: prev: {
- #      mutter = prev.mutter.overrideAttrs (old: {
- #        src = pkgs.fetchFromGitLab  {
- #          domain = "gitlab.gnome.org";
- #          owner = "vanvugt";
- #          repo = "mutter";
- #          rev = "triple-buffering-v4-46";
- #          hash = "sha256-C2VfW3ThPEZ37YkX7ejlyumLnWa9oij333d5c4yfZxc=";
- #        };
- #      });
- #    })
- #  ];
-
   systemd.services.greetd.serviceConfig = {
     Type = "idle";
     StandardInput = "tty";
