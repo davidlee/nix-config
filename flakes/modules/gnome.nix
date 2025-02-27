68c4ee1e5ac5fa39f1c62c6806b6ae8487e8c380
David Lee <dav@davlee.com>
2025-02-27 14:15:01 +1100

    wait is this working?

---
+++ flakes/modules/gnome.nix
@@ -0,0 +1,52 @@
+{pkgs, username, lib, config, ... }: {
+  
+  home-manager.users.${username} = {
+    home.packages = with pkgs; [
+      gnomeExtensions.appindicator
+    ];
+
+    # dconf.settings = {
+    #   "org/gnome/shell" = {
+    #     favorite-apps = [ "firefox.desktop" "code.desktop" ];
+    #   };
+    # };
+
+  };
+
+  programs = {
+    dconf.enable = true;
+  };
+
+  services = {
+    xserver = {
+      enable = true;
+      displayManager = {
+        gdm = {
+          # enable = true;
+          wayland = true;
+        };
+      };
+      desktopManager = {
+        gnome = {
+          enable = true;
+        };
+      };
+    };
+
+    gnome = {
+      evolution-data-server.enable = true;
+      gnome-settings-daemon.enable = true;
+      gnome-keyring.enable = true;
+      gnome-browser-connector.enable = true;
+      gnome-online-accounts.enable = true;
+    };
+    # for systray icons in gnome
+    udev.packages = with pkgs; [ gnome-settings-daemon ];
+
+    sysprof.enable = true;
+  };
+
+
+
+
+}
