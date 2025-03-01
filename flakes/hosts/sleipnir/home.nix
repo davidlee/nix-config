d39608fff8fe3ead58f649b54180a2012c3ffe8b
David Lee <dav@davlee.com>
2025-03-01 16:26:08 +1100

    add host

---
+++ flakes/hosts/sleipnir/home.nix
@@ -0,0 +1,29 @@
+{ username, inputs, ... }: {
+
+  imports = [
+    inputs.walker.homeManagerModules.default
+
+    ../../home/packages.nix
+    ../../home/programs.nix    
+    ../../home/apps.nix
+   ];
+  
+  home = {
+    username = "${username}";
+    stateVersion = "24.11";
+    sessionVariables = {
+      EDITOR = "hx";
+      DIFFPROG = "delta";
+      MANPAGER = "nvim +Man!";
+
+      XCURSOR_SIZE = 24;
+      NIXOS_OZONE_WL = 1;
+    };
+  };
+
+  # services.swayidle.enable = true;
+  services.copyq.enable = true;
+  
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+}
