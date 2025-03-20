f3f87de7bc32ab893557319a7d28a1c8869f5dd5
David Lee <dav@davlee.com>
2025-03-20 21:26:47 +1100

    fonts & rio conf

--- flakes/nixos/fonts.nix
+++ flakes/nixos/fonts.nix
@@ -24,22 +24,44 @@
       fira-code-symbols
       proggyfonts
       noto-fonts
-      nerd-fonts.hack
-      nerd-fonts.tinos
-      nerd-fonts.agave
-      nerd-fonts.profont
-      nerd-fonts.iosevka
-      nerd-fonts.hasklug
-      nerd-fonts.zed-mono
+      nerd-fonts.noto 
+      nerd-fonts.hack 
+      nerd-fonts.tinos 
+      nerd-fonts.mplus 
+      nerd-fonts.lilex 
+      nerd-fonts.arimo 
+      nerd-fonts.agave 
+      nerd-fonts._3270 
+      nerd-fonts.ubuntu 
+      nerd-fonts.monoid 
+      nerd-fonts.lekton 
+      nerd-fonts.hurmit 
+      nerd-fonts.profont 
+      nerd-fonts.monofur 
+      nerd-fonts.iosevka 
+      nerd-fonts.hasklug 
+      nerd-fonts.go-mono 
+      nerd-fonts.cousine 
+      nerd-fonts.zed-mono 
+      nerd-fonts.overpass 
       victor-mono
       terminus_font
       jetbrains-mono
       monaspace
       dina-font
       geist-font
+      
     ];
   };
+  
   fonts.fontDir.enable = true;
   fonts.enableDefaultPackages = true;
+
+  fonts.fontconfig.enable = true; 
+  fonts.fontconfig.defaultFonts.monospace = [ "JetBrainsMono Nerd Font" ];
+  fonts.fontconfig.defaultFonts.sansSerif = [ "Noto Sans" ];
+  fonts.fontconfig.defaultFonts.serif = [ "Noto Serif" ];
+  fonts.fontconfig.defaultFonts.emoji = [ "Noto Color Emoji" ];
+
 
 }
