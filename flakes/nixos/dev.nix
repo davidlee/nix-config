9ff4c9835a829790e22f74d840a9284cda43bbe2
David Lee <dav@davlee.com>
2025-05-26 17:14:43 +1000

    darwin: factor shared packages + dev + tui apps out & include them

--- flakes/nixos/dev.nix
+++ flakes/nixos/dev.nix
@@ -1,161 +1,25 @@
 { pkgs, ... } : {
   
+  imports = [
+    ../modules/shared-development.nix   
+  ];
+
   environment.systemPackages = with pkgs; [
-    ## programming - general 
-    exercism
-    tree-sitter
 
-    ## ai 
-    ollama
-    oterm
-    aichat
+    # ## ai 
     local-ai
-    mods
-    llama-cpp
-    llm
-    llm-ls
-    # vllm FIX 
-    mistral-rs
     lmstudio
+    # vllm
 
-    ## SCM
-    gh
-    gitFull
-    jujutsu
-    hub
-    delta
-    difftastic
-    diffnav
-    jujutsu
+    # ## SCM
     sublime-merge
-    meld
-    diffr
-    diffsitter
-    diffstat
-    diffedit3
-    diffoscope
-    mergiraf
-    tig
 
-    ## supervisors / runners
-    direnv
-    just
-    overmind
-    watch
-    watchman
-    watcher
-    watchexec
-    
-    ## build system
-    gnumake
-    ninja
-
-    ## www
-    html-tidy
-    prettierd
-
-    ## utility LSPs
-    lsp-ai
-    simple-completion-language-server
-    dot-language-server
-    yaml-language-server
-    tooling-language-server
-    mdx-language-server
-    docker-language-server
-    bash-language-server
-    vim-language-server
-    # just-lsp
-    postgres-lsp
-    typos-lsp
-    htmx-lsp
-    jq-lsp
-    fish-lsp
-    ctags-lsp
-
-    ## cli dev utils
-    semgrep
-    
-    ## json/yaml
-    jq
-    yq-go
-
-    ## markdown
-    markdownlint-cli
-    markdownlint-cli2
-    markdown-oxide
-    marksman
-
-    ## keyboard firmware
+    # ## keyboard firmware
     qmk-udev-rules
-    dfu-util
-    qmk
-    dtc
 
-    ## parse / lex
-    bison
-    
-    ## lang.c
-    lldb
+    # ## lang.c
     valgrind
-    lld
-    llvm
     strace
-    gcc
-    
-    ## lang.elixir 
-    lexical
 
-    ## lang.go
-    gofumpt # go formatter
-    
-    ## lang.python
-    uv
-    python313Packages.pywatchman
-    python3Packages.python-lsp-server
-    python3Packages.python-lsp-ruff
-    python3Packages.python-lsp-jsonrpc
-    python3Packages.python-lsp-black
-    python3Packages.pyls-isort
-    python3Packages.pyls-flake8
-    python3Packages.flake8
-    python3Packages.isort
-    python3Packages.black
-    jupyter-all
-
-    ## lang.odin
-    # ols broken
-
-    ## lang.clojure
-    clojure-lsp
-
-    ## lang.haskell
-    haskell-language-server
-
-    ## lang.ruby
-    ruby
-    ruby-lsp
-    bundler
-    rake
-
-    ## lang.lua
-    lua
-    lua54Packages.luarocks-nix 
-    vimPlugins.fzf-lua
-    lua-language-server
-    luarocks-packages-updater
-
-    ## lang.js
-    bun
-    pnpm
-    corepack_latest
-    nodejs_latest
-    typescript-language-server
-    typescript
-    emscripten
-    
-    ## lang.zig
-    zig
-    zls
-    zig-shell-completions
   ];
 }
--- flakes/nixos/packages.nix
+++ flakes/nixos/packages.nix
@@ -4,6 +4,11 @@
   ...
 }:
 {
+
+  imports = [
+    ../modules/shared-packages.nix   
+  ];
+
   # system packages
   # 
   environment.systemPackages = with pkgs; [
@@ -11,122 +16,28 @@
     greetd.greetd
     greetd.tuigreet
 
-    ## zsh / posix
-    zsh
-    zsh-autocomplete
-    antidote
-    shellcheck
-    shfmt
-
-    ## alt shells
-    nushell
-    oils-for-unix
-    fish
-    xonsh
-
-    ## disk / io
-    duf
+    # ## disk / io
     hdparm
     smartmontools
-    pydf
     udiskie
 
-    ## package management
-    comma
+    # ## package management
     appimage-run
-    cachix
     devcontainer
-    devenv
     flatpak
-    manix
-    nh
-    nil
-    nix-bisect
-    nix-btm
-    nixd
-    nix-diff
-    nix-direnv
-    nix-du
-    nix-index
-    nix-inspect
-    nix-melt
-    nix-output-monitor
-    nix-bash-completions
-    nix-search-cli
-    nix-search-tv
-    nix-derivation
-    nix-deploy
-    nix-forecast
-    nix-health
     nix-janitor
-    nix-top
-    nix-tree
-    nixfmt-rfc-style
-    nvd
-    pkg-config
     sd-switch
-    unixtools.nettools
-    unixtools.xxd
 
-    # network / http
-    curl
-    dig
-    httpie
-    iftop
-    inetutils
+    # # network / http
     iptraf-ng
     nethogs
-    nmap
-    netcat
-    socat
     nmon
-    openssl
-    oha
-    sn0int
-    tcpdump
-    trippy
-    unixtools.nettools
-    unixtools.xxd
     vnstat
-    wget
-    xh
-    yt-dlp
     
-    ## www
-    w3m-full
-    lynx
-    
-    ## files / find
-    fd
-    file
-    findutils
+    # ## files / find
     fsearch
-    fzf-zsh
-    fzy
-    mcfly
     plocate
-    ripgrep
-    vgrep
-    sad
-    trash-cli
-    # semgrep 
-    tree
-    zoxide
 
-    ## compression
-    xz
-    unar
-    _7zz
-    unrar
-    p7zip
-    unzip
-    gnutar
-    cpio
-
-    ## process management
-    pstree
-    killall
-    
     ## low level / system monitoring
     ccache
     cpuid
@@ -142,51 +53,19 @@
     x86info
     lm_sensors
 
-    ## unix / text
-    coreutils
-    gnused
-    gawk
-    bat
-    emacsclient-commands
-    lsd
-    eza
-    chroma
-    sd 
-
-    ## English
-    vale
-    vale-ls
-    aspell
-
     ## converters
     pandoc
 
-    ## download / backup
-    syncthing
-    backblaze-b2
-    aria2
+    # ## download / backup
     backintime
 
-    ## docs, notes, productivity
-    tealdeer
-    pinfo
+    # ## docs, notes, productivity
     zeal
     dnote
-    remind
-
-    ## graphing
-    d2
-    graphviz
 
     ## image / graphics / multimedia
-    ffmpeg
     glfw
-    pastel
     viu
-
-    ## fonts
-    nerd-font-patcher
-    fontconfig
 
     ## audio
     alsa-utils
@@ -198,44 +77,16 @@
     playerctl
     
     ## security / crypto / secrets
-    nvdtools
-    seclists
-    git-crypt
-    gpgme
-    gpg-tui
-    oath-toolkit
-    _1password-cli
     keepassxc
 
-    ## servers
-    caddy
-    sqlite
-    postgresql
-
     ## tasks
-    tasksh
-    taskwarrior3
     taskchampion-sync-server
 
     ## frivolity
-    neofetch
-    fastfetch
-    figlet
     fortune
     cmatrix
     nms
     openrgb-with-all-plugins
-
-    ### libs
-    glib
-    libffi
-    libiconv
-    libyaml
-    openssl
-    openssl.dev
-    pkg-config
-    raylib
-    zlib
 
     # windows API libs
     dotnet-runtime
