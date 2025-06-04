248246c72043622f830761dce28b7530a8037777
David Lee <dav@davlee.com>
2025-06-04 15:23:04 +1000

    refactor: consolidate bindings

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -9,13 +9,11 @@ require("which-key").add({
   -- add headings
   { "<leader>f", group = "File" },
   { "<leader>b", group = "Buffer" },
-  { "<leader>s", group = "Snacks picker" },
   { "<leader>g", group = "Git" },
   { "<leader>s", group = "Search" },
-  { "<leader>u", group = "Toggles" },
+  { "<leader>u", group = "Toggle" },
 })
 
--- TODO: export table for snacks.vim to keep stuff all in this file
 --
 ----------------------------------------------------------------------------------------------------
 ---
@@ -25,6 +23,84 @@ require("which-key").add({
 -- (o)per (t)erm (l)ang
 -- '' :map = nvo
 -- ! :map! = ic
+
+-- export table to keep all binds in this file, as best we can
+local Keys = {
+  snacks = {
+    keys = {
+      -- Toggles
+      { "<leader>ut", function() Snacks.terminal.toggle() end, desc = "Toggle terminal" },
+      { "<leader>u.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+      -- Favourite Pickers & Explorer (single char bindings)
+      { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+      { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
+      { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+      { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+      { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+      { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+      { '<leader>R"', function() Snacks.picker.registers() end, desc = "Registers" },
+      -- find
+      { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+      {
+        "<leader>fc",
+        function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end,
+        desc = "Find Config File",
+      },
+      { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+      { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+      { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+      { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
+      -- git
+      { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches" },
+      { "<leader>gl", function() Snacks.picker.git_log() end, desc = "Git Log" },
+      { "<leader>gL", function() Snacks.picker.git_log_line() end, desc = "Git Log Line" },
+      { "<leader>gs", function() Snacks.picker.git_status() end, desc = "Git Status" },
+      { "<leader>gS", function() Snacks.picker.git_stash() end, desc = "Git Stash" },
+      { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff (Hunks)" },
+      { "<leader>gf", function() Snacks.picker.git_log_file() end, desc = "Git Log File" },
+      -- Grep
+      -- search
+      { "<leader>s/", function() Snacks.picker.search_history() end, desc = "Search History" },
+      { "<leader>sa", function() Snacks.picker.autocmds() end, desc = "Autocmds" },
+      { "<leader>sb", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+      { "<leader>sB", function() Snacks.picker.grep_buffers() end, desc = "Grep Open Buffers" },
+      { "<leader>sc", function() Snacks.picker.command_history() end, desc = "Command History" },
+      { "<leader>sC", function() Snacks.picker.commands() end, desc = "Commands" },
+      { "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+      { "<leader>sD", function() Snacks.picker.diagnostics_buffer() end, desc = "Buffer Diagnostics" },
+      { "<leader>sg", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>sw", function() Snacks.picker.grep_word() end, desc = "Vis selection or word", mode = { "n", "x" } },
+      { "<leader>sh", function() Snacks.picker.help() end, desc = "Help Pages" },
+      { "<leader>sH", function() Snacks.picker.highlights() end, desc = "Highlights" },
+      { "<leader>si", function() Snacks.picker.icons() end, desc = "Icons" },
+      { "<leader>sj", function() Snacks.picker.jumps() end, desc = "Jumps" },
+      { "<leader>sk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
+      { "<leader>sl", function() Snacks.picker.loclist() end, desc = "Location List" },
+      { "<leader>sm", function() Snacks.picker.marks() end, desc = "Marks" },
+      { "<leader>sM", function() Snacks.picker.man() end, desc = "Man Pages" },
+      { "<leader>sp", function() Snacks.picker.lazy() end, desc = "Search for Plugin Spec" },
+      { "<leader>sq", function() Snacks.picker.qflist() end, desc = "Quickfix List" },
+      { "<leader>sR", function() Snacks.picker.resume() end, desc = "Resume" },
+      { "<leader>su", function() Snacks.picker.undo() end, desc = "Undo History" },
+      { "<leader>uC", function() Snacks.picker.colorschemes() end, desc = "Colorschemes" },
+      -- LSP
+      { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+      { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+      { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+      { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+      { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+      { "<leader>ss", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+      { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+      -- Toggle
+      { "<leader>uz", function() Snacks.zen() end, desc = "Toggle zen mode" },
+      { "<leader>uZ", function() Snacks.zen.zoom() end, desc = "Toggle zen zoom mode" },
+      -- bufdelete
+      { "<leader>bd", function() Snacks.bufdelete() end, desc = "Buffer > delete" },
+      -- toggle terminal
+    },
+  },
+}
 
 --- Normal Mode bindings:
 -- X: delete char without yank
@@ -55,9 +131,11 @@ bind("n", "<space>!", "<cmd>write<cr>", { desc = "Save" })
 -- f-: (:Triptych)
 bind("n", "<leader>f-", "<cmd>Triptych<cr>", { silent = true, desc = "Toggle Triptych" })
 -- fo: (:Oil)
-bind("n", "<leader>fo", "<cmd>Oil<cr>", { silent = true, desc = "Oil" })
+bind("n", "<leader>fo", "<cmd>Oil<cr>", { silent = true, desc = "Oil (:Oil)" })
 -- fE: (:Ex) Explore with NetRW
 bind("n", "<leader>fE", "<cmd>Ex<cr>", { desc = "NetRW (:Ex)" })
+-- fy: (:Yazi) open in Yazi
+bind("n", "<leader>fy", "<cmd>Yazi<cr>", { desc = "Yazi (:Yazi)" })
 
 --- move lines:
 -- normal mode
@@ -69,3 +147,5 @@ bind("i", "<A-k>", "<esc>:m .-2<cr>==gi", { desc = "Move line up" })
 -- visual mode
 bind("v", "<A-j>", "<cmd>m '>+1<cr>gv=gv", { desc = "Move selected lines down" })
 bind("v", "<A-k>", "<cmd>m '<-2<cr>gv=gv", { desc = "Move selected lines up" })
+
+return Keys
--- .config/nvim/lua/plugins/file_managers.lua
+++ .config/nvim/lua/plugins/file_managers.lua
@@ -3,6 +3,19 @@
 --
 -- note: see snacks.lua for Snacks.picker & Snacks.explorer, our main file manager
 
+-- yazi
+require("lze").load({
+  "yazi.nvim",
+  after = function()
+    --
+    require("plenary")
+    require("yazi")
+  end,
+})
+
+require("mini.deps").add({ source = "antosha417/nvim-lsp-file-operations" })
+require("mini.deps").now(function() require("lsp-file-operations").setup() end)
+
 --
 require("lze").load({
   "triptych.nvim",
@@ -12,22 +25,14 @@ require("lze").load({
         nav_left = { "h", "<left>" },
         nav_right = { "l", "i", "<right>", "<CR>" }, -- If target is a file, opens the file in-place
       },
-      -- keys = {
-      --   { "<leader>-", ":Triptych<CR>" },
-      -- },
     })
   end,
 })
 
--- //
-
 -- neo-tree really doesn't want to be lazy loaded
 require("neo-tree").setup({
-  -- without this, tabbing between netrw / snacks.picker will open a bonus neotree
+  -- NOTE: without this, tabbing between netrw / snacks.picker will open a bonus neotree
   -- regardless of whether we've required neo-tree.
-  --
-  -- I'd like to know
-  --
   filesystem = {
     hijack_netrw_behavior = "disabled",
   },
