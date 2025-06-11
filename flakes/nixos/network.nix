42592ea07170f36f33a68b4ee4447a5bb7f63a8b
David Lee <dav@davlee.com>
2025-06-11 22:06:09 +1000

    tidies

--- flakes/nixos/network.nix
+++ flakes/nixos/network.nix
@@ -4,7 +4,6 @@
   pkgs,
   ...
 }: {
-
   # network manager frontends
   environment.systemPackages = with pkgs; [
     nm-tray
@@ -13,31 +12,31 @@
   networking = {
     networkmanager.enable = true;
     hostName = hostname;
-    timeServers = options.networking.timeServers.default ++ [ "pool.ntp.org" ];
+    timeServers = options.networking.timeServers.default ++ ["pool.ntp.org"];
 
-    # firewall 
+    # firewall
     firewall = {
       enable = true;
-      allowedTCPPorts = [ 22 80 443 ];
+      allowedTCPPorts = [22 80 443];
     };
 
     nftables.enable = true;
-    
+
     # for tailscale:
-    nameservers = [ "100.100.100.100" "1.1.1.1" ];
-    search = [ "bandicoot-sunfish.ts.net" ];
+    nameservers = ["100.100.100.100" "1.1.1.1"];
+    search = ["bandicoot-sunfish.ts.net"];
   };
 
   # Odin hung from Yggdrassil for nine nights
-  
-  programs.ssh.startAgent = true;
 
   services = {
+    fail2ban.enable = true;
     openssh = {
       enable = true;
       settings = {
         GatewayPorts = "yes";
         PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
         X11Forwarding = false;
         PermitRootLogin = "no";
         UseDns = true;
