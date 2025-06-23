cfbcaedf25a40719bde0aa156500261c5955c1e7
David Lee <dav@davlee.com>
2025-06-23 14:12:52 +1000

    feat: lsp configs (unmodified examples) & zk.nvim

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,11 @@
 local md = require("mini.deps")
 local lz = require("lze")
 
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#zls
+
+-- nix
+vim.lsp.enable("nixd")
+
 -- markdown
 lz.load({
 
@@ -10,7 +15,7 @@ lz.load({
       require("lspconfig")["marksman"].setup({
         --
       })
-      require("lspconfig")["luals"].setup({})
+      require("lspconfig")["lua_ls"].setup({})
     end,
   },
 })
@@ -19,3 +24,120 @@ lz.load({
 md.add({ source = "soulis-1256/eagle.nvim" })
 
 md.later(function() require("eagle").setup({}) end)
+
+-- lua
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md#lua_ls
+vim.lsp.config("lua_ls", {
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if
+        path ~= vim.fn.stdpath("config")
+        and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+      then
+        return
+      end
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most
+        -- likely LuaJIT in the case of Neovim)
+        version = "LuaJIT",
+        -- Tell the language server how to find Lua modules same way as Neovim
+        -- (see `:h lua-module-load`)
+        path = {
+          "lua/?.lua",
+          "lua/?/init.lua",
+        },
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+          -- Depending on the usage, you might want to add additional paths
+          -- here.
+          -- '${3rd}/luv/library'
+          -- '${3rd}/busted/library'
+        },
+        -- Or pull in all of 'runtimepath'.
+        -- NOTE: this is a lot slower and will cause issues when working on
+        -- your own configuration.
+        -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+        -- library = {
+        --   vim.api.nvim_get_runtime_file('', true),
+        -- }
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
+})
+
+-- marksman
+vim.lsp.enable("marksman")
+
+-- rust
+vim.lsp.config("rust_analyzer", {
+  settings = {
+    ["rust-analyzer"] = {
+      diagnostics = {
+        enable = false,
+      },
+    },
+  },
+})
+
+-- zls / zig
+vim.lsp.enable("zls")
+
+-- zk
+vim.lsp.enable("zk")
+
+-- tailwindCSS
+vim.lsp.enable("tailwindcss")
+
+-- TOML / taplo
+vim.lsp.enable("taplo")
+
+-- text
+vim.lsp.enable("textlsp")
+vim.lsp.enable("vale_ls")
+
+-- typescript
+vim.lsp.config("ts_ls", {
+  init_options = {
+    plugins = {
+      {
+        name = "@vue/typescript-plugin",
+        location = "/usr/local/lib/node_modules/@vue/typescript-plugin",
+        languages = { "javascript", "typescript", "vue" },
+      },
+    },
+  },
+  filetypes = {
+    "javascript",
+    "typescript",
+    -- "vue",
+  },
+})
+
+-- eslint
+vim.lsp.enable("eslint")
+
+-- css variables
+vim.lsp.enable("css_variables")
+
+-- basics (buffer,path,snippets)
+vim.lsp.enable("basics_ls")
+
+-- bash
+vim.lsp.enable("bashls")
+
+-- ast-grep
+vim.lsp.enable("ast_grep")
+
+-- yaml
+vim.lsp.enable("yamlls")
---
+++ .config/nvim/todo.md
@@ -0,0 +1,15 @@
+## neovim config todo
+
+### FIXME
+
+### LSP / etc
+- [ ] luals 
+- [ ] LSP / formatter for React / TypeScript / etc
+- [ ] magic closing tags for HTML / TSX
+
+### other
+- [x] set up or fix: move line up / down @done(06/21/25 13:58)
+  - it's Alt-[j/k] not Ctrl-[j/k]
+- [ ] ! learn / config: fancy editing of surrounding braces etc
+  - mini-surround in keymap.lua:109
+  - [ ] TODO: try it out with a working LSP
