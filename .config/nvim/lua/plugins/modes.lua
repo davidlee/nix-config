8227c08f51ece594c0c3bf02be2720aa18d23715
David Lee <dav@davlee.com>
2025-07-29 10:37:35 +1000

    config: mostly break-reminder

--- .config/nvim/lua/plugins/modes.lua
+++ .config/nvim/lua/plugins/modes.lua
@@ -37,3 +37,21 @@ md.later(function()
     },
   })
 end)
+
+md.add("ray-x/go.nvim")
+md.later(function()
+  require("go").setup({})
+  local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function() require("go.format").goimports() end,
+    group = format_sync_grp,
+  })
+end)
+
+md.add("terrastruct/d2-vim")
+md.later(function()
+  -- require("d2") --.setup({
+  --ft = "d2",
+  --})
+end)
