403fd875ed40e2ddb439f37e447bec496f2a7589
David Lee <dav@davlee.com>
2025-10-14 09:30:07 +1100

    add npins

--- flakes/nixos/pkg.nix
+++ flakes/nixos/pkg.nix
@@ -7,6 +7,9 @@
   environment.systemPackages = with pkgs; [
     inputs.nix-search-tv.packages.x86_64-linux.default
 
+    # ## https://jade.fyi/blog/pinning-nixos-with-npins/
+    npins
+
     # ## package management
     appimage-run
     devcontainer
