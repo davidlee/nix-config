6b906a40af416dd2014c034be3b5def6ee730c62
David Lee <dav@davlee.com>
2025-10-16 01:25:51 +1100

    add env hax

--- nix-config/nix-up.sh
+++ nix-config/nix-up.sh
@@ -9,6 +9,11 @@ shift
 nixpkgs_pin=$(nix eval --raw -f npins/default.nix nixpkgs)
 nix_path="nixpkgs=${nixpkgs_pin}:nixos-config=${PWD}/hosts/Sleipnir/configuration.nix"
 
+if [[ $cmd == "env" ]]; then
+    echo "$nix_path"
+    exit 0
+fi
+
 # without --fast, nixos-rebuild will compile nix and use the compiled nix to
 # evaluate the config, wasting several seconds
 sudo env NIX_PATH="${nix_path}" nixos-rebuild "$cmd" --no-reexec "$@"
