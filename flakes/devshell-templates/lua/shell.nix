cae74a72153061b3fdd053ef9105fcf99a13a299
David Lee <dav@davlee.com>
2025-10-18 20:34:10 +1100

    starship not in nix

---
+++ flakes/devshell-templates/lua/shell.nix
@@ -0,0 +1,16 @@
+{
+  pkgs ? import <nixpkgs> { },
+  treefmt ? null,
+}:
+let
+  inherit (pkgs) lib;
+in
+pkgs.mkShell {
+  packages = with pkgs; [
+  lua-language-server
+  stylua
+  selene
+  lua5_1
+  lua51Packages.luarocks
+  ];
+}
