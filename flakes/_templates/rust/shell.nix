d6fcaf2e8bffc0582bd9f885fd6c35ee22ff620e
David Lee <dav@davlee.com>
2025-10-20 19:21:51 +1100

    rf: rust shell

--- flakes/_templates/rust/shell.nix
+++ flakes/_templates/rust/shell.nix
@@ -1,16 +1,15 @@
-{
-  pkgs ?
-    import <nixpkgs> {
-      config = {};
-      overlays = [
-        (import (
-          builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"
-        ))
+let
+  rustball = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz";
+in
+  {
+    pkgs ?
+      import <nixpkgs> {
+        config = {};
+        overlays = [(import rustball)];
+      },
+  }:
+    pkgs.mkShell {
+      packages = with pkgs; [
+        rust-bin.beta.latest.default
       ];
-    },
-}:
-pkgs.mkShell {
-  packages = with pkgs; [
-    rust-bin.beta.latest.default
-  ];
-}
+    }
