ba9aef7ba082ed7701127334fcfdd56f635ff3e5
David Lee <dav@davlee.com>
2026-01-22 12:41:56 +1100

    dev config

--- dev/wub/shell.nix
+++ dev/wub/shell.nix
@@ -6,24 +6,3 @@ with import <nixpkgs> {};
         pry
       ]
   )
-# with (import <nixpkgs> {}); let
-#   gems = bundlerEnv {
-#     name = "your-package";
-#     inherit ruby;
-#     gemdir = ./.;
-#   };
-# in
-#   stdenv.mkDerivation {
-#     name = "your-package";
-#     buildInputs = [gems ruby];
-#   }
-# {pkgs ? import <nixpkgs> {}}:
-# pkgs.mkShell {
-#   packages = with pkgs; [
-#     ruby_3_5
-#   ];
-#
-#   shellHook = ''
-#   '';
-# }
-
