8ce2f87bf99aa604b2273e688e8b5c75d138d436
David Lee <dav@davlee.com>
2026-05-12 00:17:17 +1000

    emacs: pimp my themes

--- flakes/modules/home/emacs.nix
+++ flakes/modules/home/emacs.nix
@@ -1,54 +1,50 @@
 _: {
-  flake.homeModules.emacs =
-    {
-      pkgs,
-      inputs,
-      username,
-      ...
-    }:
-    let
-      emacsDir = ../../../.emacs.d; # relative to repo root
-      configDirs = [
-        "core"
-        "apps"
-        "lang"
-        "lisp"
-        "editing"
-        "completion"
-        "org"
-        "dev"
-      ];
-      elFilesIn =
-        dir:
-        let
-          path = emacsDir + "/${dir}";
-        in
-        if builtins.pathExists path then
-          map (f: path + "/${f}") (
-            builtins.filter (f: builtins.match ".*\\.el" f != null) (builtins.attrNames (builtins.readDir path))
-          )
-        else
-          [ ];
-      allElFiles = builtins.concatLists (map elFilesIn configDirs);
-      config = builtins.concatStringsSep "\n" (map builtins.readFile allElFiles);
-
-      emacs = pkgs.emacsWithPackagesFromUsePackage {
-        package = pkgs.emacs-unstable-pgtk;
-        inherit config;
-        alwaysEnsure = true;
-      };
+  flake.homeModules.emacs = {
+    pkgs,
+    inputs,
+    username,
+    ...
+  }: let
+    emacsDir = ../../../.emacs.d; # relative to repo root
+    configDirs = [
+      "core"
+      "apps"
+      "lang"
+      "lisp"
+      "editing"
+      "completion"
+      "org"
+      "dev"
+    ];
+    elFilesIn = dir: let
+      path = emacsDir + "/${dir}";
     in
-    {
-      nixpkgs.overlays = [ inputs.emacs-overlay.overlays.default ];
+      if builtins.pathExists path
+      then
+        map (f: path + "/${f}") (
+          builtins.filter (f: builtins.match ".*\\.el" f != null) (builtins.attrNames (builtins.readDir path))
+        )
+      else [];
+    allElFiles = builtins.concatLists (map elFilesIn configDirs);
+    config = builtins.concatStringsSep "\n" (map builtins.readFile allElFiles);
 
-      home.packages = [
-        emacs
-        pkgs.emacsclient-commands
-      ];
-
-      services.emacs = {
-        enable = true;
-        package = emacs;
-      };
+    emacs = pkgs.emacsWithPackagesFromUsePackage {
+      package = pkgs.emacs-unstable-pgtk;
+      inherit config;
+      alwaysEnsure = true;
     };
+  in {
+    nixpkgs.overlays = [inputs.emacs-overlay.overlays.default];
+
+    home.packages = [
+      emacs
+      pkgs.emacsclient-commands
+      # pkgs.emacsPackages.treesit-auto
+    ];
+
+    services.emacs = {
+      enable = true;
+      package = emacs;
+    };
+  };
 }
