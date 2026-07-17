c5cb2021066e25fd1ed20ccae26e41dfb576e9a6
David Lee <dav@davlee.com>
2026-07-17 20:32:12 +1000

    tidies

--- flakes/modules/home/shared/nushell.nix
+++ flakes/modules/home/shared/nushell.nix
@@ -1,21 +1,4 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}: let
-  # oh-my-posh's home-manager module, for oh-my-posh >=26, execs `oh-my-posh init nu`
-  # on every shell startup, which itself rewrites
-  # ~/.local/share/nushell/vendor/autoload/oh-my-posh.nu as a side effect. nu
-  # auto-sources that file at startup, before config.nu runs. Two nu logins starting
-  # close together (e.g. tmux opening several panes/windows at once) can race on that
-  # write and leave a torn file, which then fails to parse before config.nu gets a
-  # chance to regenerate it. Generating the init script once at build time (the
-  # pre-v26 module behaviour) removes the runtime write entirely.
-  ohMyPoshNuInit = pkgs.runCommand "oh-my-posh-nushell-config.nu" {} ''
-    ${lib.getExe config.programs.oh-my-posh.package} init nu --config ${config.xdg.configHome}/oh-my-posh/config.json --print >> "$out"
-  '';
-in {
+{pkgs, ...}: {
   programs = {
     nushell = {
       enable = true;
@@ -25,7 +8,6 @@ in {
 
       # this is loaded pretty early in proceedings ...
       extraConfig = ''
-        source ${ohMyPoshNuInit}
         source ~/nushell/config.nu
       '';
 
@@ -56,8 +38,7 @@ in {
     # but I lost enthusiasm
     oh-my-posh = {
       enable = true;
-      # nushell integration is hand-rolled above (build-time init, no runtime write)
-      enableNushellIntegration = false;
+      enableNushellIntegration = true;
       enableBashIntegration = true;
       settings = builtins.fromJSON (
         builtins.readFile ../../../files/omp.custom.json
